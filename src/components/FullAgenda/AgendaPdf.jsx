fe659974fc57e8ba017ba6541c950dda5af5c3fd
emammad22 <mammadoveltun22@gmail.com>
2024-02-27 10:08:01 +0400

    restore

--- src/components/FullAgenda/AgendaPdf.jsx
+++ src/components/FullAgenda/AgendaPdf.jsx
@@ -6,6 +6,7 @@ import { useRef } from "react";
 import logo from "../../assets/dsa-logo.png";
 
 export default function AgendaPdf() {
+  console.log(1)
   const componentRef = useRef(null);
   const [componentHeight, setComponentHeight] = useState(0);
   const agendaRef = useRef();
